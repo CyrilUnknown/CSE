80387ca2e24b5f3c9ec9f42b747b0b658b1b9701
cyril cummings <bh6a@wetigers.org>
2018-10-24 14:38:45 -0700

    idc

---
+++ Notes/Basic Python Notes.py
@@ -0,0 +1,48 @@
+print("Hello World")
+
+# Apparently I'm going to slow, so I will speed up
+# This is a comment
+# This has no effect on the code
+# This is used for a variety of things, such as
+# 1. Making personal notes about my code
+# 2. Commenting out code that does not work
+
+print("Notice what is happening here.")
+print()  # This creates a new line
+print()  #Do you notice the underline here?
+# Hover over top it to see what is wrong.
+
+# Math
+print(5 + 3)
+print(5 - 3)
+print(4 * 5)
+print(5 / 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+car_name = "Weibe mobile"  # String
+car_name + "Tesla"  # String
+car_cylinders = 16
+
+
+
+
+
+# Taking Input
+name = input("What is your name? ")
+print("Hello %s" % name)
+
+age = input("How old are you?")
+print("%s?" you )
