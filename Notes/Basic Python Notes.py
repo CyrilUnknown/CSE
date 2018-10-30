a900dc5f7867cb54c7e8d0649c81aa9b032be14c
cyril cummings <bh6a@wetigers.org>
2018-10-30 14:37:42 -0700

    notes

--- Notes/Basic Python Notes.py
+++ Notes/Basic Python Notes.py
@@ -1,3 +1,4 @@
+"""
 print("Hello World!")
 
 # Apparently I'm going to slow, so I will speed up
@@ -48,5 +49,43 @@ age = input("How old are you?")
 print("%s? You belong in a museum!" % age)
 
 # Recasting
-real_age = int(input("How old are you again? "))
+real_age = int(input("You are such a bastord? "))
 hidden_age = real_age + 5
+print(hidden_age)
+"""
+
+# Multi-line Comments
+
+"""
+This is a multi-line comment
+anything in between them is automatically commented out.
+"""
+
+
+# Defining Functions
+def say_it():
+    print("Hello World!")
+
+
+say_it()
+say_it()
+say_it()
+
+
+# f(x) =2x + 3
+def f(x):
+
+
+
+f(1)
+f(5)
+f(5000)
+
+
+def distance(x1, y1, x2, y2):
+    dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+    print(dist)
+
+
+distance(0, 0, 3, 4)
+distance(0, 0, 5, 12)
