2a2579b6fcc7d64132785c11625704d98f56f2c0
cyril cummings <bh6a@wetigers.org>
2018-11-13 14:36:56 -0800

    guthub

--- Notes/lucky 7s.py
+++ Notes/lucky 7s.py
@@ -1,4 +1,13 @@
 import random
 money_total = 15
-Random = random.randint(1, 6)
-Dice1 = random.randint(1, 6)
+rounds_total = 0
+while money_total > 0:
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    add = dice1 + dice2
+    rounds_total += 1
+    money_total -= 1
+    if add == 7:
+        money_total += 100
+    print(money_total)
+    print("You have played %s rounds" % rounds_total)
