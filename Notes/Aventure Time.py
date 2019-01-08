928716e92148dba9bbe11402719a70b318250e32
CyrilUnknown <42391777+CyrilUnknown@users.noreply.github.com>
2019-01-08 14:39:54 -0800

    my own game
    world of oz

---
+++ Notes/Aventure Time.py
@@ -0,0 +1,8 @@
+print("Welcome To The World Of Oz!!")
+print("June 22nd 1983 someone kidnapped your daughter")
+print("she was walking home from school she was only 13 years old.")
+print("When you figured out she was dead")
+print("you need to find the guy who did this a finish him.")
+print("This is going to be a bumpy road")
+money_total = 100
+print("You have 100 dollars spend it wisely")
---
+++ Notes/semester 2 notes.py
@@ -0,0 +1,17 @@
+print("Hello World")
+# I love you guys hahahaha.
+
+Cars = 5
+Driving = True
+
+
+print("I have %s cars" % Cars)
+
+input("How old are you?")
+print("I have " + str(Cars) + " cars.")
+age = input("How old are you?")
+print("%s?? Really???" % age)
+
+
+colors = ["Yellow", "Green", "Blue", "Orange", "Purple"]
+colors.append("Brown")
