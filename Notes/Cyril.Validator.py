296dfa1dd538558cef9049f5795396e90df2af3a
CyrilUnknown <42391777+CyrilUnknown@users.noreply.github.com>
2019-05-02 14:25:48 -0700

    Sorry

--- Notes/Cyril.Validator.py
+++ Notes/Cyril.Validator.py
@@ -1,24 +1,22 @@
-test_num = "1947333886071750"
-number1 = "1947333886071750"
-number_list = list(number1)
-print(number_list)
-print(len(number_list))
-
-if len(number_list) is not 16:
-    print("It doesn't pass")
-
-my_list = number_list[0:15][:: -1]
-for index in range(len(my_list)):
-    my_list[index] = int(my_list[index])
-    if index % 2 == 0:
-        my_list[index] *= 2
-        if my_list[index] >= 9:
-            my_list[index] -= 9
-print(my_list)
+test_num = "4508878827358837"
+number_list1 = list(test_num)
+print(number_list1)
 
 
 def validate(num: str):
-    pass
+    if len(number_list1) is not 16:
+        print("It doesn't pass")
+        return False
+    the_last_number = int(num[15])
+    my_list = number_list1[0:15][:: -1]
+    for index in range(len(my_list)):
+        my_list[index] = int(my_list[index])
+        if index % 2 == 0:
+            my_list[index] *= 2
+            if my_list[index] > 9:
+                my_list[index] -= 9
+    total = sum(my_list)
+    return total % 10 == the_last_number
 
 
 print(validate(test_num))
